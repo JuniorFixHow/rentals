88a93a6986934eb1141b9fc2c8e3ddf5e1b0928b
JuniorFixHow <robertjuniorannan@gmail.com>
2024-11-01 23:14:43 +0000

    fixed container size 5

--- features/CarPayment.tsx
+++ features/CarPayment.tsx
@@ -31,16 +31,16 @@ const CarPayment = ({car}:{car:CarProps}) => {
             car?.rented ? 
             <NotAvailable car={car} />
             :
-            <div className='flex flex-col text-white gap-6 p-4 mt-6 md:mt-0 rounded-2xl  w-[18rem] md:w-full bg-[#080D1B]' >
+            <div className='flex flex-col text-white gap-6 p-4 mt-6 md:mt-0 rounded-2xl  grow m-4 bg-[#080D1B]' >
                 <div className="flex flex-col md:flex-row gap-3 items-start justify-between">
                     <div className="flex flex-col w-full md:w-[45%] gap-2">
                         <span className='text-[0.8rem] text-slate-500' >Pick-up date and time</span>
-                        <input className='bg-[#232B3F] rounded-lg px-1 py-3 text-white text-[0.8rem]' min={today(new Date())} onChange={(e)=>setStart(new Date(e.target.value))} type="datetime-local" />
+                        <input className='bg-[#232B3F] rounded-lg w-full px-1 py-3 text-white text-[0.8rem]' min={today(new Date())} onChange={(e)=>setStart(new Date(e.target.value))} type="datetime-local" />
                     </div>
 
                     <div className="flex flex-col w-full md:w-[45%] gap-2">
                         <span className='text-[0.8rem] text-slate-500' >Drop-off date and time</span>
-                        <input className='bg-[#232B3F] rounded-lg px-1 py-3 text-white text-[0.8rem]' min={today(start)} onChange={(e)=>setEnd(new Date(e.target.value))} type="datetime-local" />
+                        <input className='bg-[#232B3F] rounded-lg w-full px-1 py-3 text-white text-[0.8rem]' min={today(start)} onChange={(e)=>setEnd(new Date(e.target.value))} type="datetime-local" />
                     </div>
                 </div>
 
