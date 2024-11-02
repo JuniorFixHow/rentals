8eba740fbc78c5fff609a1d3314cd381fb31af00
JuniorFixHow <robertjuniorannan@gmail.com>
2024-11-02 15:34:40 +0000

    homepage 3

--- misc/ServiceRight.tsx
+++ misc/ServiceRight.tsx
@@ -1,18 +1,15 @@
-'use client'
-import React from 'react'
 import { ServiceLeftProps } from './ServiceLeft'
 import Image from 'next/image'
-import { useTheme } from '@/context/ThemeContext'
 
 const ServiceRight = ({serviceCard}:ServiceLeftProps) => {
-    const {isDark} = useTheme()
   return (
     <div className='bg-slate-100 hidden dark:bg-slate-800 md:flex relative rounded-lg p-3' >
         
-        <div className="flex absolute right-0 top-0 p-2 text-white dark:text-[#0C111D] bg-[#0C111D] dark:bg-white shadow rounded-full justify-center items-center">
+        <div className="flex absolute -right-1 -top-1 p-2 text-white dark:text-[#0C111D] bg-[#0C111D] dark:bg-white shadow rounded-full justify-center items-center">
             0{serviceCard?.id}
         </div>
-        <Image height={500} width={300} alt='service' src={isDark? serviceCard?.imageDark:serviceCard?.image}  />
+        {/* <div className={'h-[25rem] bg-black w-[20rem]'} ></div> */}
+        <Image height={500} width={300} alt='service' src={ serviceCard?.image}  />
     </div>
   )
 }
