a44532931e494ae0b77be1602b9a91bed5909671
JuniorFixHow <robertjuniorannan@gmail.com>
2024-11-04 20:27:36 +0000

    providers 5

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,6 +1,9 @@
 'use client'
+import ButtonNav from '@/components/ButtonNav'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
 import { ThemeProvider } from '@/context/ThemeContext'
-import { ContentProvider } from '@/misc/ContentProvider'
 import { ClerkProvider } from '@clerk/nextjs'
 import React, { ReactNode } from 'react'
 
@@ -8,9 +11,15 @@ export function Providers({children}:{children:ReactNode}) {
   return (
     <ThemeProvider>
         <ClerkProvider>
-          <ContentProvider>
+          <div className="flex flex-col dark:text-white bg-white dark:bg-[#0D0F11] font-inter">
+            <Header/>
+            <div className="flex flex-row gap-4 mt-4">
+            <Navbar/>
             {children}
-          </ContentProvider>
+            </div>
+            <ButtonNav/>
+            <Footer/>
+        </div>
         </ClerkProvider>
     </ThemeProvider>
   )
