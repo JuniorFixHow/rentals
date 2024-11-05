6f546e2ca7ba73699bebbd8d54120290406306f5
JuniorFixHow <robertjuniorannan@gmail.com>
2024-11-05 12:45:53 +0000

    dark service 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
-// layout.tsx
-// "use client"
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import Header from "@/components/Header";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import ButtonNav from "@/components/ButtonNav";
-import ThemeProvider from "@/context/ThemeContext";
-// import MainProvider from "@/components/Main";
+
 import Providers from "./Providers";
-// import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,20 +21,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       {/* Wrap entire layout in ClerkProvider */}
       {/* <ClerkProvider> */}
-        <ThemeProvider>
           <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
             <Providers>
               {children}
             </Providers>
           </body>
-        </ThemeProvider>
       {/* </ClerkProvider> */}
     </html>
   );
